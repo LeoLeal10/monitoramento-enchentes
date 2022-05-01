4ac0aacc405f15ec0f8b5ef00958f48b7729cbdc
LeoLeal10 <leo.ps335@gmail.com>
2022-05-01 12:22:32 -0300

    Add projeto MVC

---
+++ Monitoramento de enchentes/Monitoramento de enchentes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Monitoramento_de_enchentes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
